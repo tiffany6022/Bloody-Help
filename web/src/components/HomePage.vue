b9377e014eb7efdecf9bd9a977e690e63751a0fb
yiz <ql3895619@gmail.com>
2020-10-12 22:28:02 +0800

    access file or photo

--- web/src/components/HomePage.vue
+++ web/src/components/HomePage.vue
@@ -18,6 +18,8 @@
       i.smile.outline.large.icon(:class="{ green: 'normal' == health_status, outline: 'normal' != health_status }")
 
   .ui.vertical.big.segments
+    .ui.inverted.active.dimmer(v-if='loading')
+      .ui.text.loader(v-if='loading') loading
     .ui.horizontal.segments
       .ui.segment #[i.heartbeat.large.yellow.icon] 收縮壓(SYS)
       .ui.right.aligned.segment
@@ -33,13 +35,16 @@
 
 
   .container
-    #camera
-      button.icon.ui.button.circular.massive.item(@click='$emit("turnOnCamera")')
+    // #camera
+    //   button.icon.ui.button.circular.massive.item(@click='$emit("turnOnCamera")')
+    //     i.file.image.icon
+    #image
+      button.icon.ui.button.circular.massive.item(@click='handleFile')
         i.icon.camera
 </template>
 
 <script>
-
+const axios = require('axios')
 const months = [
   { text: 'Jan', id: 1 },
   { text: 'Feb', id: 2 },
@@ -52,76 +57,78 @@ const months = [
   { text: 'Sep', id: 9 },
   { text: 'Oct', id: 10 },
   { text: 'Nov', id: 11 },
-  { text: 'Dec', id: 12 },
+  { text: 'Dec', id: 12 }
 ]
 
-export default{
-
-  data() { return {
-    year: 2020,
-    month: 0,
-    date: 0,
-    daylist: [],
-    monthlist: months,
-    blood_pressure1: '',
-    blood_pressure2: '',
-    blood_pressure3: '',
-    health_status: 'none',
-  }},
+export default {
+  data() {
+    return {
+      year: 2020,
+      month: 0,
+      date: 0,
+      daylist: [],
+      monthlist: months,
+      blood_pressure1: '',
+      blood_pressure2: '',
+      blood_pressure3: '',
+      health_status: 'none',
+      img: '',
+      loading: false
+    }
+  },
 
   computed: {
     status_word() {
       if (this.health_status == 'normal') {
         return '血壓處於正常範圍'
-      }
-      else if (this.health_status == 'elevated') {
+      } else if (this.health_status == 'elevated') {
         return '高血壓前期，需要盡早開始控制血壓'
-      }
-      else if (this.health_status == 'high') {
+      } else if (this.health_status == 'high') {
         return '建議應與醫師討論如何追蹤及是否治療'
-      }
-      else {
+      } else {
         return '請填入血壓值'
       }
-    },
+    }
   },
 
-  mounted () {
+  mounted() {
     var today = new Date()
     this.year = today.getFullYear()
-    this.month = today.getMonth() + 1    // 0~11 month
+    this.month = today.getMonth() + 1 // 0~11 month
     this.date = today.getDate()
-    this.$root.$on("writedata", (res) => {
-      this.blood_pressure1 = res.SYS
-      this.blood_pressure2 = res.DIA
-      this.blood_pressure3 = res.PUL
-      this.setStorage()
-    })
   },
 
-  updated () {
+  updated() {
     this.jump(this.date)
   },
 
   watch: {
-    'date' () {
+    date() {
       this.getLocalData()
     },
 
-    'month' () {
+    month() {
       this.daylist = []
       var d = new Date(this.year, this.month, 0)
       var days = d.getDate() // how many days in this month
-      for(var i=1; i<=days; i++){
-        var w = new Date(this.year, this.month-1, i)
+      for (var i = 1; i <= days; i++) {
+        var w = new Date(this.year, this.month - 1, i)
         var week = w.getDay() // 0~6(SUN~SAT)
-        if (week == 0){ week = "SUN" }
-        else if (week == 1){ week = "MON" }
-        else if (week == 2){ week = "TUE" }
-        else if (week == 3){ week = "WED" }
-        else if (week == 4){ week = "THU" }
-        else if (week == 5){ week = "FRI" }
-        else if (week == 6){ week = "SAT" }
+        if (week == 0) {
+          week = 'SUN'
+        } else if (week == 1) {
+          week = 'MON'
+        } else if (week == 2) {
+          week = 'TUE'
+        } else if (week == 3) {
+          week = 'WED'
+        } else if (week == 4) {
+          week = 'THU'
+        } else if (week == 5) {
+          week = 'FRI'
+        } else if (week == 6) {
+          week = 'SAT'
+        }
         this.daylist.push({
           date: i,
           week: week
@@ -130,30 +137,71 @@ export default{
       this.getLocalData()
     },
 
-    'blood_pressure1' () {
+    blood_pressure1() {
       this.setStorage()
     },
-    'blood_pressure2' () {
+    blood_pressure2() {
       this.setStorage()
     },
-    'blood_pressure3' () {
+    blood_pressure3() {
       this.setStorage()
-    },
+    }
   },
 
   methods: {
+    async handleFile() {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = () => {
+          this.upload(reader.result)
+        }
+
+        const input = document.createElement('input')
+        input.type = 'file'
+
+        input.onchange = () => {
+          const file = input.files[0]
+          if (!file.type.match(/image.*/)) return reject()
+
+          reader.readAsDataURL(file)
+          resolve(input.files[0])
+        }
+
+        input.click()
+      })
+    },
+
+    async upload(base64data) {
+      this.loading = true
+
+      var image = {
+        base64: base64data,
+        name: 'imageName'
+      }
+
+      this.blood_pressure1 = '...'
+      this.blood_pressure2 = '...'
+      this.blood_pressure3 = '...'
+
+      axios.post('/img', image).then(res => {
+        this.blood_pressure1 = res.data.SYS
+        this.blood_pressure2 = res.data.DIA
+        this.blood_pressure3 = res.data.PUL
+
+        this.loading = false
+      })
+    },
 
     getLocalData() {
       var dateString = this.getDateString()
       var healthData = JSON.parse(window.localStorage.getItem(dateString))
       if (healthData) {
-        console.log("have localStorage!")
+        console.log('have localStorage!')
         this.blood_pressure1 = healthData.blood_pressure1
         this.blood_pressure2 = healthData.blood_pressure2
         this.blood_pressure3 = healthData.blood_pressure3
         this.assessStatus()
-      }
-      else {
+      } else {
         this.blood_pressure1 = ''
         this.blood_pressure2 = ''
         this.blood_pressure3 = ''
@@ -165,9 +213,13 @@ export default{
       var healthData = {
         blood_pressure1: this.blood_pressure1,
         blood_pressure2: this.blood_pressure2,
-        blood_pressure3: this.blood_pressure3,
+        blood_pressure3: this.blood_pressure3
       }
-      if(( healthData.blood_pressure1 ) || ( healthData.blood_pressure2 ) || ( healthData.blood_pressure3 )){
+      if (
+        healthData.blood_pressure1 ||
+        healthData.blood_pressure2 ||
+        healthData.blood_pressure3
+      ) {
         var dateString = this.getDateString()
         window.localStorage.setItem(dateString, JSON.stringify(healthData))
       }
@@ -182,28 +234,23 @@ export default{
     },
 
     jump(date) {
-      document.getElementById("d"+date).scrollIntoView({ inline: 'center' })
+      document.getElementById('d' + date).scrollIntoView({ inline: 'center' })
     },
 
     assessStatus() {
-      if( this.blood_pressure1 && this.blood_pressure2 ){
-        if( this.blood_pressure1 < 120 && this.blood_pressure2 < 80 ){
+      if (this.blood_pressure1 && this.blood_pressure2) {
+        if (this.blood_pressure1 < 120 && this.blood_pressure2 < 80) {
           this.health_status = 'normal'
-        }
-        else if( this.blood_pressure1 < 140 && this.blood_pressure2 < 90 ){
+        } else if (this.blood_pressure1 < 140 && this.blood_pressure2 < 90) {
           this.health_status = 'elevated'
-        }
-        else{
+        } else {
           this.health_status = 'high'
         }
-      }
-      else{
+      } else {
         this.health_status = 'none'
       }
-    },
-
-  },
-
+    }
+  }
 }
 </script>
 
@@ -257,5 +304,4 @@ export default{
     display: flex
     justify-content: space-around
     width: 100%
-
 </style>
